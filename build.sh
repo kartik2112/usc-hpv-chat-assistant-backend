b0ddd90663ab7aa93bd7f130861346fea43e28b0
Kartik Shenoy <kartik.shenoyy@gmail.com>
2026-03-21 11:07:10 -0700

    Updated requirements

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# pip install --upgrade pip
-pip install -r requirements.txt
-python -m spacy download en_core_web_lg
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+
+# Install dependencies with uv
+uv pip install -r requirements.txt --system
+
+# Download spaCy model
+python -m spacy download en_core_web_md
